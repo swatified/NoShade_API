bf147720b32f8160d149c1f80c75887c69230b7e
Swati Sharma <scarletshukurenai@gmail.com>
2025-01-21 17:59:31 +0530

    fixing numpy issue pt2

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,4 +2,5 @@ import multiprocessing
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-timeout = 120
+timeout = 600
+preload_app = True
--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+export PATH="/opt/python/3.10.15/bin:$PATH"
+export PYTHONPATH="/opt/python/3.10.15/lib/python3.10/site-packages:$PYTHONPATH"
+
 python -m pip install --upgrade pip
+pip install numpy==1.24.3
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-gunicorn API.wsgi:application --bind=0.0.0.0:8000
+gunicorn API.wsgi:application --bind=0.0.0.0:8000 --timeout 600 --workers 4
